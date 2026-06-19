7d1e33927c2f5fac5b55f5b1b2513fe56bff8f73
Lucian Ciuraru <ciuraru.lucian8@yahoo.com>
2026-06-19 14:15:01 +0100

    Set up project structure and add TaskItem model

---
+++ CLI/CommandParser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTrackerCLIProject.CLI
+{
+    internal class CommandParser
+    {
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTrackerCLIProject.Models
+{
+    internal class TaskItem
+    {
+    }
+}
