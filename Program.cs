605b6831a3c219af6f187a1a8a7c6b841a1db65a
Ciuraru Lucian <ciuraru.lucian8@yahoo.com>
2026-06-24 13:10:11 +0100

    feat: implement and manually verify AddTask logic in TaskServices

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TaskTrackerCLIProject.Services;
+using TaskTrackerCLIProject.Storage;
+
+namespace TaskTrackerCLIProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            JsonStorage js = new JsonStorage("tasks.json");
+            TaskServices ts = new TaskServices(js);
+            var hardCodedTask = ts.AddTask("Buy Groceries");
+            Console.WriteLine($"Id:{hardCodedTask.Id} ");
+            Console.WriteLine($"Description: {hardCodedTask.Description} ");
+            Console.WriteLine($"Status: {hardCodedTask.Status} ");
+        }
+
+    }
+}
--- Services/TaskServices.cs
+++ Services/TaskServices.cs
@@ -15,7 +15,53 @@ namespace TaskTrackerCLIProject.Services
             _storage = storage;
         }
 
-        public void AddTask() { }
+        public TaskItem AddTask(string description) 
+        {
+            List<TaskItem> tasks = _storage.LoadTasks();
+            try
+            {
+                bool hasTasks = tasks.Any();
+                if (hasTasks)
+                {
+                    int newId = tasks.Max(task => task.Id) + 1;
+                    var newTask = new TaskItem
+                    {
+                        Id = newId,
+                        Description = description,
+                        Status = "todo",
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now
+                    };
+
+                    tasks.Add(newTask);
+                    _storage.SaveTasks(tasks);
+
+                    return newTask;
+                }
+                else
+                {
+                    var newTask = new TaskItem
+                    {
+                        Id = 1,
+                        Description = description,
+                        Status = "todo",
+                        CreatedAt = DateTime.Now,
+                        UpdatedAt = DateTime.Now
+                    };
+
+                    tasks.Add(newTask);
+                    _storage.SaveTasks(tasks);
+
+                    return newTask;
+                }
+            }catch (InvalidOperationException ex)
+            {
+                throw new Exception("Error: There are no elements in the list", ex);
+            }catch (Exception ex)
+            {
+                throw new Exception("Error: An error has occured, the list wasn't saved", ex);
+            }
+        }
 
         public void UpdateTask() { }
 
