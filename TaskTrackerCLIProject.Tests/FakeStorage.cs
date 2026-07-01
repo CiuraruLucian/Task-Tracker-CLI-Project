ee29bd55fff9e5f6813338b8321a9686f14f129d
Ciuraru Lucian <ciuraru.lucian8@yahoo.com>
2026-07-01 14:46:52 +0100

    test: move test project into repo and push to GitHub

---
+++ TaskTrackerCLIProject.Tests/FakeStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskTrackerCLIProject.Storage;
+using TaskTrackerCLIProject.Models;
+
+namespace TaskTrackerCLIProject.Tests
+{
+    public class FakeStorage : IStorage
+    {
+        private List<TaskItem> _testingTasks = new List<TaskItem>();
+        public void SaveTasks(List<TaskItem> tasks)
+        {
+            _testingTasks = tasks;
+
+        }
+
+        public List<TaskItem> LoadTasks()
+        {
+            return _testingTasks;
+        }
+    }
+}
